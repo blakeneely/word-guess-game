51fddb18d758603ba40278e5e3d0fe9055d669de
Blake Neely <blakeneely21@gmail.com>
2019-08-08 09:27:22 -0500

    added logic to restart round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,27 +10,38 @@ var wordBank = [
     "demogorgon",
     "eggos",
     "eighties",
-    "new coke",
     "experiment",
     "russians",
-    "scoops ahoy",
     "hawkins",
     "indiana",
     "ghostbusters",
-    "arcade"
+    "arcade",
+    "halloween"
 ];
 
 var word = "";
 var answerArray = [];                       // Holds "_" and fills with correct letters guessed
 var wrongGuesses = [];                      // Holds guessed letters
-var wordLetters = []; 
-var remainingLetters = 9;         // 
+var wordLetters = [];                       // Holds random word split into an array
+var remainingLetters = 9;                   // Number of incorrect guesses 
 var wins = 0;
 var losses = 0;
 var answerArrayText = document.getElementById("answer-array-text");
 var remainingLettersText = document.getElementById("remaining-letters-text");
 var wrongGuessesText = document.getElementById("guessed-letters-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
 
+function roundStart() {
+    answerArray = [];
+    wrongGuesses = [];
+    wordLetters = [];
+    remainingLetters = 9;
+    wrongGuessesText.textContent = "Incorrect guesses: " + wrongGuesses.join(" ");
+    getRandomWord();
+    makeAnswerArray();
+
+}
 
 function getRandomWord() {                          //Function to pull random word from wordbank and assign to variable word
     word = wordBank[Math.floor(Math.random() * wordBank.length)];
@@ -47,18 +58,36 @@ function checkGuess(array, element) {               // Finds wrong guesses and p
     if (array.indexOf(element) === -1) {
         wrongGuesses.push(element);
         remainingLetters--;
-        wrongGuessesText.textContent = "Letters you have guessed: " + wrongGuesses.join(" ");
+        wrongGuessesText.textContent = "Incorrect guesses: " + wrongGuesses.join(" ");
         roundOver();
     }
-}
+    else if (array.indexOf(element) !== -1){       // Finds right guesses and loops them into answerArray
+        // If user guess is correct
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === element) {
+                // display correct letter in answer
+                answerArray[j] = element;
+                // subtract from remaining letters left to guess
+                roundOver();
+            }
+            answerArrayText.textContent = answerArray.join(" ");
+        }
+    };       
+};
 function roundOver(){
-    if (answerArray == wordLetters) {
-        alert("You win!");
+    if (answerArray.toString() == wordLetters.toString()) {
+        wins++;
+        alert("You win! The correct word was " + word);
+        winsText.textContent = "Wins: " + wins;
+        roundStart();
     }
-    else if (remainingLetters = 0) {
-        alert("You lose!");
+    else if (remainingLetters == 0) {
+        losses++;
+        alert("You lose! Sorry, the correct word was " + word);
+        lossesText.textContent = "Losses: " + losses;
+        roundStart();
     }
-}
+};
 
 // Display number of letters left to guess
 
@@ -70,16 +99,6 @@ function roundOver(){
         var guess = event.key.toLowerCase();
         checkGuess(wordLetters, guess);
         
-        // If user guess is correct
-        for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                // display correct letter in answer
-                answerArray[j] = guess;
-                // subtract from remaining letters left to guess
-                roundOver();
-            }
-            answerArrayText.textContent = answerArray.join(" ");
-        }
     };
 
 // ********* Main Game *********
